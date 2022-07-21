e9c146c3ed01fbfd6142bf859328c820b19cb0b1
jihunlm <jihunhk2@illinois.edu>
2022-07-20 22:41:31 -0500

    Add my project

--- log_in.cpp
+++ log_in.cpp
@@ -4,27 +4,75 @@
 
 using namespace std;
 
-bool CheckLogIn ()
+struct Login {
+		bool result;
+		string userID;
+	};
+
+auto CheckLogIn ()
 {
 	string userID, password, un, pw;
-	
 
-	cout << "Type in the User ID"; 
+	cout << "Type in the User ID :\n "; 
 	cin >> userID;
-	cout << "Type in the Password";
-	cin >> password
+	cout << "Type in the Password :\n";
+	cin >> password;
 	
-	ifstream read("Project_2//" + useID + ".txt");
+	ifstream read(userID + ".txt");
 	getline(read, un);
-	getlint(read, pw);
+	getline(read, pw);
 
-	if(un == userID) && pw == password) {
-		return true;
+	if(un == userID && pw == password) {
+		return Login {true, userID};
 	}
 	else {
-		return false;
+		return Login {false, userID};
 	}
 }
 
 int main() {
-	
+	int choice;	
+
+	cout << "1: Register \n 2: Login \n Your Choice : "; 
+	cin >> choice;
+
+	if (choice == 1) {
+		string username, password, data;
+		cout << "Select a user name :\n";
+		cin >> username;
+		cout << "Select a password :\n";
+		cin >> password;
+		cout << "Type in the information\n";
+		cin.ignore();
+		getline(cin, data);
+
+		ofstream file(username + ".txt");
+
+		file << username <<endl << password << endl << data << endl;
+		file.close();
+
+		main(); //return to main to register and login
+	}
+	else if(choice == 2)
+	{	
+		auto [status, userID] = CheckLogIn();
+		if(!status) {
+			cout << "False login!" << endl;
+			system("PAUSE");
+			return 0;
+		}
+		else {
+			string USERID, password, data;
+			cout << "Successfully logged in!" << endl;
+			cout << "The data : \n";
+			ifstream read (userID + ".txt");
+			getline(read, USERID);
+			getline(read, password);
+			getline(read, data);
+			cout << data << endl;
+			system("PAUSE");
+			return 1;
+		}
+	}
+}
+
